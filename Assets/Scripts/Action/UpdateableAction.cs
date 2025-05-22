42a02fd7723e89d13d8c886d9a1a043406f928a3
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-23 02:10:08 +0900

    feat: UpdateableAction 추가
    gameContext 내의 tickDispatcher에서 해당 IUpdatable을 다룰 수 있게 updataHandlers에 해당 entity를 붙이는 클래스

---
+++ Assets/Scripts/Action/UpdateableAction.cs
@@ -0,0 +1,26 @@
+public class UpdateableAction : IAction
+{
+    public UpdateableAction()
+    {
+
+    }
+
+    public void Attach(GameContext gameContext, Entity entity, int priority)
+    {
+        gameContext.updateHandlers.Add(entity);
+    }
+
+    public void Detach(GameContext gameContext, Entity entity)
+    {
+        gameContext.updateHandlers.Remove(entity);
+    }
+
+    public bool CanExecute(GameContext gameContext, Entity entity, float deltaTIme)
+    {
+        return true;
+    }
+
+    public void Execute(GameContext gameContext, Entity entity, float deltaTIme)
+    {
+    }
+}
