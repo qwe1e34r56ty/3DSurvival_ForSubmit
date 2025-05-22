64276b6cd0fa0ad81f3ddb2e990083c48116abf3
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-23 02:10:51 +0900

    feat: UIStaminarBarAction 추가
    UI에서 Staminar를 표시하게 함

---
+++ Assets/Scripts/Action/UIAction/UIStaminaBarAction.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIStaminaBarAction : IAction
+{
+    private Dictionary<GameObject, Image> barImages;
+
+    public UIStaminaBarAction()
+    {
+        barImages = new();
+    }
+
+    public void Attach(GameContext context, 
+        Entity entity, 
+        int priority)
+    {
+        Transform barTransform = entity.gameObject.transform.Find("Canvas/Conditions/Stamina/StaminaBar");
+        if (barTransform == null)
+        {
+            Logger.LogWarning($"[UIStaminaBarAction] [{entity.gameObject.name}] StaminaBar Object not found.");
+            return;
+        }
+        barImages.Add(entity.gameObject, barTransform.GetComponent<Image>());
+    }
+
+    public void Detach(GameContext context, Entity entity)
+    {
+        barImages.Remove(entity.gameObject);
+    }
+
+    public bool CanExecute(GameContext context, Entity entity, float deltaTIme)
+    {
+        if (context.controllableEntity == null)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void Execute(GameContext context, Entity entity, float deltaTIme)
+    {
+        if (!barImages.TryGetValue(entity.gameObject, out var image))
+        {
+            return;
+        }
+        float Stamina = context.controllableEntity.GetStat(StatID.Stamina) ?? 0;
+        float maxStamina = context.controllableEntity.GetStat(StatID.MaxStamina) ?? 0;
+        if(maxStamina == 0)
+        {
+            return;
+        }
+        image.fillAmount = Mathf.Clamp01(Stamina / maxStamina);
+    }
+}
