fa5777bba67b0f755abe8e68407e1d32246d4ea7
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-23 02:04:27 +0900

    feat: UseSpeedBoostAction 추가
    Speed Boost Item 사용 Action

---
+++ Assets/Scripts/Action/UseSpeedBoostAction.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseSpeedBoostAction : IAction
+{
+    private Dictionary<Entity, Coroutine> activeEffects;
+    private float defaultSpeedBoostDuration = 5f;
+
+    public UseSpeedBoostAction()
+    {
+        activeEffects = new();
+    }
+
+    public bool CanExecute(GameContext gameContext, Entity entity, float deltaTime)
+    {
+        return true;
+    }
+
+    public void Attach(GameContext gameContext, Entity entity, int priority)
+    {
+
+    }
+
+    public void Detach(GameContext gameContext, Entity entity)
+    {
+        if (activeEffects.TryGetValue(entity, out Coroutine routine))
+        {
+            gameContext.coroutineHandler.StopRunningCoroutine(routine);
+            activeEffects.Remove(entity);
+        }
+    }
+
+    public void Execute(GameContext gameContext, Entity entity, float deltaTime)
+    {
+        if (!Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            return;
+        }
+
+        float? speedBoostItemCount = entity.GetStat(ItemID.SpeedBoost);
+        if (speedBoostItemCount.HasValue && speedBoostItemCount.Value >= 1)
+        {
+            float currentMoveSpeedScale = entity.GetStat(StatID.MoveSpeedScale) ?? 1f;
+            entity.SetStat(StatID.MoveSpeedScale, currentMoveSpeedScale * 4);
+            entity.SetStat(ItemID.SpeedBoost, speedBoostItemCount.Value - 1);
+            Logger.Log($"[SpeedBoost] used : [{entity.gameObject.name}]");
+
+            if (activeEffects.TryGetValue(entity, out Coroutine existing))
+            {
+                gameContext.coroutineHandler.StopRunningCoroutine(existing);
+                Logger.Log($"[SpeedBoost] refreshed : [{entity.gameObject.name}]");
+            }
+
+            float duration = entity.GetStat(StatID.SpeedBoostDuration) ?? defaultSpeedBoostDuration;
+            Coroutine newEffect = gameContext.coroutineHandler.RunCoroutine(RemoveEffectAfterDuration(gameContext, entity, duration));
+            activeEffects[entity] = newEffect;
+        }
+    }
+
+    private IEnumerator RemoveEffectAfterDuration(GameContext gameContext, Entity entity, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+
+        float currentMoveSpeedScale = entity.GetStat(StatID.MoveSpeedScale) ?? 1f;
+        entity.SetStat(StatID.MoveSpeedScale, currentMoveSpeedScale / 4);
+        activeEffects.Remove(entity);
+        Logger.Log($"[SpeedBoost] effect off : [{entity.gameObject.name}]");
+    }
+}
