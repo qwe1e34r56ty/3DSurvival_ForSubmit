bbb06f175d7ee1bf68ce54530c59a94757c34e08
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-23 02:05:54 +0900

    feat: MovableAction 추가

---
+++ Assets/Scripts/Action/MoveableAction.cs
@@ -0,0 +1,27 @@
+public class MoveableAction : IAction
+{
+    public MoveableAction()
+    {
+
+    }
+
+    public void Attach(GameContext gameContext, Entity entity, int priority)
+    {
+
+    }
+
+    public void Detach(GameContext gameContext, Entity entity)
+    {
+
+    }
+
+    public bool CanExecute(GameContext gameContext, Entity entity, float deltaTIme)
+    {
+        return true;
+    }
+
+    public void Execute(GameContext gameContext, Entity entity, float deltaTIme)
+    {
+
+    }
+}
