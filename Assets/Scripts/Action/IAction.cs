15bc179b1323726b07534e328ca3bf2964dcadb9
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-23 02:06:59 +0900

    feat: IAction 추가
    Entity들의 행동(Action) 클래스의 인터페이스

---
+++ Assets/Scripts/Action/IAction.cs
@@ -0,0 +1,12 @@
+using System.Security.Cryptography.X509Certificates;
+
+public interface IAction
+{
+    // Action을 Entity에 붙일 때 발생해야 하는 동작
+    public void Attach(GameContext gameContext, Entity entity, int priority);
+    // Action을 Entity에서 떼어낼 때 발생해야 하는 동작
+    public void Detach(GameContext gameContext, Entity entity);
+    public bool CanExecute(GameContext gameContext, Entity entity, float deltaTime);
+    // UpdateableAction 붙어있는 Entity에서 동작
+    public void Execute(GameContext gameContext, Entity entity, float deltaTime);
+}
