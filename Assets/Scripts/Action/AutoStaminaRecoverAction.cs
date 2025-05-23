ea5b48a2789df964d3a2144b1d463afdad265ed1
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-23 10:03:37 +0900

    feat: AutoStaminaRecoverAction 추가
    스태미너 회복

---
+++ Assets/Scripts/Action/AutoStaminaRecoverAction.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoStaminaRecoverAction : IAction
+{
+    public AutoStaminaRecoverAction()
+    {
+
+    }
+
+    public void Attach(GameContext gameContext, Entity entity, int priority)
+    {
+
+    }
+
+    public void Detach(GameContext gameContext, Entity entity)
+    {
+
+    }
+
+    public bool CanExecute(GameContext gameContext, Entity entity, float deltaTime)
+    {
+        return true;
+    }
+
+    public void Execute(GameContext gameContext, Entity entity, float deltaTime)
+    {
+        float? stamina = entity.GetStat(StatID.Stamina);
+        float? maxStamina = entity.GetStat(StatID.MaxStamina);
+        float? autoStaminaRecover = entity.GetStat(StatID.AutoStaminaRecover);
+        if (!stamina.HasValue)
+        {
+            return; 
+        }
+        if (!maxStamina.HasValue)
+        {
+            return;
+        }
+        if (!autoStaminaRecover.HasValue)
+        {
+            return;
+        }
+        if (stamina > maxStamina)
+        {
+            return;
+        }
+        entity.SetStat(StatID.Stamina, 
+            Mathf.Min(
+                stamina.Value + autoStaminaRecover.Value * deltaTime, 
+                maxStamina.Value
+                )
+            );
+    }
+}
